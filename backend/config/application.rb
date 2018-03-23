3556fc11adc3eb69dbb8cb864ea8bed109db0e59
Michal Kwiatkowski <michal@trivas.pl>
2018-03-23 10:54:26 +0100

    Configure CORS for the backend.

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -1,16 +1,12 @@
 require_relative 'boot'
 
 require "rails"
-# Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
-# require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
-# require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -27,5 +23,13 @@ module Backend
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Configure CORS for all routes.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
